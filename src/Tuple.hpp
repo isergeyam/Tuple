99764df755b96a6a2d1c534af5566cba55956806
Grigoryants Sergey <isergey.am@gmail.com>
2018-05-02 16:09:38 +0300

    Tuple almost done. CMakeLists added

--- src/Tuple.hpp
+++ src/Tuple.hpp
@@ -1,6 +1,7 @@
 #pragma once
 #include <cstddef>
 #include <utility>
+template <typename... Types> class Tuple;
 template <size_t... Indices> struct IndexSequence {
   using type = IndexSequence<Indices...>;
 };
@@ -20,9 +21,11 @@ using MakeIndexSequence_t = typename MakeIndexSequence<N>::type;
 template <size_t, typename T> struct TupleElement {
   T value_;
   TupleElement() = default;
-  template <typename U>
+  template <typename U,
+            typename = std::enable_if_t<std::is_convertible<U, T>::value>>
   explicit TupleElement(U &&value_) : value_(std::forward<U>(value_)) {}
 };
+template <typename... Types> struct IsTuple : std::false_type {};
 template <typename Sequences, typename... Types> struct TupleImpl;
 template <typename> struct IsTupleImpl : std::false_type {};
 template <size_t... Indices, typename... Types>
@@ -33,37 +36,6 @@ template <template <class> class Obj, typename Head, typename... Tail>
 constexpr bool IsAnyOf() {
   return Obj<Head>::value || IsAnyOf<Obj, Tail...>();
 }
-template <size_t... Indices, typename... Types>
-struct TupleImpl<IndexSequence<Indices...>, Types...>
-    : TupleElement<Indices, Types>... {
-  TupleImpl() = default;
-  template <typename... OtherTypes,
-            typename = std::enable_if_t<
-                !IsAnyOf<IsTupleImpl, std::decay_t<OtherTypes>...>()>>
-  explicit TupleImpl(OtherTypes &&... values)
-      : TupleElement<Indices, Types>(std::forward<OtherTypes>(values))... {}
-};
-template <typename... Types>
-class Tuple
-    : public TupleImpl<MakeIndexSequence_t<sizeof...(Types)>, Types...> {
-  using Base_t = TupleImpl<MakeIndexSequence_t<sizeof...(Types)>, Types...>;
-  using InnerTypes = TypeSequence<Types...>;
-
-public:
-  Tuple() = default;
-  Tuple(const Tuple &) = default;
-  Tuple(Tuple &&) = default;
-  Tuple &operator=(const Tuple &) = default;
-  Tuple &operator=(Tuple &&) = default;
-  template <typename... OtherTypes>
-  explicit Tuple(OtherTypes &&... values)
-      : Base_t(std::forward<OtherTypes>(values)...) {}
-  void swap(Tuple &other) {
-    Tuple tmp = other;
-    other = std::move(*this);
-    *this = std::move(tmp);
-  }
-};
 template <class T> struct unwrap_refwrapper { using type = T; };
 
 template <class T> struct unwrap_refwrapper<std::reference_wrapper<T>> {
@@ -86,6 +58,88 @@ template <size_t I, typename Head, typename... Tail> struct TypeAtIndex {
 };
 template <size_t I, typename... Types>
 using TypeAtIndex_t = typename TypeAtIndex<I, Types...>::type;
+template <size_t I, typename... Types>
+constexpr TypeAtIndex_t<I, Types...> &get(Tuple<Types...> &tup) noexcept;
+template <typename T, typename... Types>
+constexpr T const &get(Tuple<Types...> const &t) noexcept;
+template <typename T, typename... Types>
+constexpr T &&get(Tuple<Types...> &&t) noexcept;
+template <class...> struct conjunction : std::true_type {};
+template <class B1> struct conjunction<B1> : B1 {};
+template <class B1, class... Bn>
+struct conjunction<B1, Bn...>
+    : std::conditional_t<bool(B1::value), conjunction<Bn...>, B1> {};
+template <class...> struct disjunction : std::false_type {};
+template <class B1> struct disjunction<B1> : B1 {};
+template <class B1, class... Bn>
+struct disjunction<B1, Bn...>
+    : std::conditional_t<bool(B1::value), B1, disjunction<Bn...>> {};
+template <size_t Val1, size_t Val2> struct IsEqual : std::false_type {};
+template <size_t Val1> struct IsEqual<Val1, Val1> : std::true_type {};
+template <size_t... Indices, typename... Types>
+struct TupleImpl<IndexSequence<Indices...>, Types...>
+    : TupleElement<Indices, Types>... {
+  TupleImpl() = default;
+  template <
+      typename... OtherTypes,
+      typename = std::enable_if_t<sizeof...(Types) == sizeof...(OtherTypes)>>
+  explicit TupleImpl(OtherTypes &&... values)
+      : TupleElement<Indices, Types>(std::forward<OtherTypes>(values))... {}
+  /*template <
+      typename... OtherTypes,
+      typename = std::enable_if_t<!IsEqual<sizeof...(OtherTypes), 1>::value>>
+  explicit TupleImpl(OtherTypes &&... values)
+      : TupleElement<Indices, Types>(std::forward<OtherTypes>(values))... {}*/
+};
+template <typename... Types>
+class Tuple
+    : public TupleImpl<MakeIndexSequence_t<sizeof...(Types)>, Types...> {
+  using Base_t = TupleImpl<MakeIndexSequence_t<sizeof...(Types)>, Types...>;
+  using InnerTypes = TypeSequence<Types...>;
+
+public:
+  Tuple() = default;
+  Tuple(const Tuple &) = default;
+  Tuple(Tuple &&) = default;
+  Tuple &operator=(const Tuple &) = default;
+  Tuple &operator=(Tuple &&) = default;
+  static constexpr size_t Size = sizeof...(Types);
+  template <typename... OtherTypes>
+  explicit Tuple(OtherTypes &&... values)
+      : Base_t(std::forward<OtherTypes>(values)...) {}
+  void swap(Tuple &other) {
+    Tuple tmp = other;
+    other = std::move(*this);
+    *this = std::move(tmp);
+  }
+  /*template <typename... OtherTypes, size_t... Indices>
+  explicit Tuple(Tuple<OtherTypes...> &&other, IndexSequence<Indices...>)
+      : Base_t(std::forward<TypeAtIndex_t<Indices, OtherTypes...>(get<Indices>(
+                   std::forward<Tuple<OtherTypes...>>(other)))...>) {}
+  template <typename... OtherTypes>
+  explicit Tuple(Tuple<OtherTypes...> &&other)
+      : Tuple(std::forward<Tuple<OtherTypes...>>(other),
+              MakeIndexSequence<Size>()) {}
+  template <typename... OtherTypes>
+  constexpr bool operator<(const Tuple<OtherTypes...> &other) {
+    for (size_t i = 0; i < Size; ++i) {
+      if (get<i>(*this) < get<i>(other))
+        return true;
+      else if (get<i>(*this) > get<i>(other))
+        return false;
+    }
+    return false;
+  }
+  template <typename... OtherTypes>
+  constexpr bool operator==(const Tuple<OtherTypes...> &other) {
+    for (size_t i = 0; i < Size; ++i) {
+      if (get<i>(*this) != get<i>(other))
+        return false;
+    }
+    return true;
+  }*/
+};
+template <typename... Types> constexpr size_t Tuple<Types...>::Size;
 template <size_t I, typename... Types>
 constexpr TypeAtIndex_t<I, Types...> &get(Tuple<Types...> &tup) noexcept {
   TupleElement<I, TypeAtIndex_t<I, Types...>> &base = tup;
@@ -131,4 +185,55 @@ constexpr T &&get(Tuple<Types...> &&t) noexcept {
   static_assert(CountType<T, Types...>() == 1,
                 "Exactly one type expected in Tuple types.");
   return get<FindType<T, Types...>()>(std::move(t));
+}
+template <typename Tuple1, size_t... Indices1, typename Tuple2,
+          size_t... Indices2>
+auto TwoTuplesCatHelper(Tuple1 &&t1, IndexSequence<Indices1...>, Tuple2 &&t2,
+                        IndexSequence<Indices2...>) {
+  return MakeTuple(get<Indices1>(std::forward<Tuple1>(t1))...,
+                   get<Indices2>(std::forward<Tuple2>(t2))...);
+}
+template <typename Tuple1, typename Tuple2>
+constexpr auto TwoTuplesCat(Tuple1 &&t1, Tuple2 &&t2) {
+  return TwoTuplesCatHelper(
+      std::forward<Tuple1>(t1),
+      MakeIndexSequence<std::remove_reference_t<Tuple1>::Size>(),
+      std::forward<Tuple2>(t2),
+      MakeIndexSequence<std::remove_reference_t<Tuple2>::Size>());
+}
+template <typename _Tp, typename _Up, size_t __i, size_t __size>
+struct __TupleCompare {
+  static constexpr bool __eq(const _Tp &__t, const _Up &__u) {
+    return bool(get<__i>(__t) == get<__i>(__u)) &&
+           __TupleCompare<_Tp, _Up, __i + 1, __size>::__eq(__t, __u);
+  }
+
+  static constexpr bool __less(const _Tp &__t, const _Up &__u) {
+    return bool(get<__i>(__t) < get<__i>(__u)) ||
+           (!bool(get<__i>(__u) < get<__i>(__t)) &&
+            __TupleCompare<_Tp, _Up, __i + 1, __size>::__less(__t, __u));
+  }
+};
+
+template <typename _Tp, typename _Up, size_t __size>
+struct __TupleCompare<_Tp, _Up, __size, __size> {
+  static constexpr bool __eq(const _Tp &, const _Up &) { return true; }
+
+  static constexpr bool __less(const _Tp &, const _Up &) { return false; }
+};
+template <typename... Types1, typename... Types2>
+constexpr bool operator==(const Tuple<Types1...> &t1,
+                          const Tuple<Types2...> &t2) {
+  static_assert(sizeof...(Types1) == sizeof...(Types2),
+                "Only tuples of same length can be compared");
+  return __TupleCompare<Tuple<Types1...>, Tuple<Types2...>, 0,
+                        sizeof...(Types1)>::__eq(t1, t2);
+}
+template <typename... Types1, typename... Types2>
+constexpr bool operator<(const Tuple<Types1...> &t1,
+                         const Tuple<Types2...> &t2) {
+  static_assert(sizeof...(Types1) == sizeof...(Types2),
+                "Only tuples of same length can be compared");
+  return __TupleCompare<Tuple<Types1...>, Tuple<Types2...>, 0,
+                        sizeof...(Types1)>::__less(t1, t2);
 }
--- test/TupleTest.cpp
+++ test/TupleTest.cpp
@@ -5,6 +5,11 @@ TEST(TupleTest, Constructor) {
   EXPECT_EQ(get<0>(mtpl), 1);
   EXPECT_EQ(get<0>(mtpl), 1);
   EXPECT_EQ(get<0>(mtpl), 1.);
+  Tuple<int, int, double> other(mtpl);
+  EXPECT_EQ(mtpl, other);
+  Tuple<int> t1(0);
+  Tuple<int> t2(t1);
+  EXPECT_EQ(t1, t2);
 }
 TEST(TupleTest, Assignment) {
   Tuple<int, double, double> mtpl(1, 2.4, 2.5);
@@ -15,6 +20,11 @@ TEST(TupleTest, GetByType) {
   Tuple<int, double, double> mtpl(3, 2, 1);
   EXPECT_EQ(get<int>(mtpl), 3);
   // get<double>(mtpl); <- static assertion failed.
+}
+TEST(TupleTest, TupleCat) {
+  Tuple<int, int, double> mt1(1, 1, 1.);
+  Tuple<float, float, int> mt2(1., 1., 1);
+  EXPECT_TRUE(TwoTuplesCat(mt1, mt2) == MakeTuple(1, 1, 1., 1., 1., 1));
 }
 int main(int argc, char **argv) {
   ::testing::InitGoogleTest(&argc, argv);
